7aa6269131f4d7223d6e9672215eca55b4d92d6f
Saiful Rahman <sr.perante@gmail.com>
2021-11-18 00:52:41 +0700

    update lokasi asli

--- lokasi.php
+++ lokasi.php
@@ -28,7 +28,7 @@ require_once('session_check.php');
 	<!-- end header -->
 	<div id="lokasi">
 		<div class="container">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d643.4412595441248!2d114.21753470034427!3d-7.7531013859039755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd12bc52ae61be3%3A0x472ab0b3e98cad11!2sKedai%20NANI!5e0!3m2!1sen!2sid!4v1635177939556!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.7888200916645!2d113.9484001139741!3d-7.705796778461482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7275e891056ed%3A0x8ac2e19e40caacb2!2sJhei.id!5e0!3m2!1sid!2sid!4v1637171385621!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 		</div>
 	</div>
 
